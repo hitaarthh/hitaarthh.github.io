0ac5fc03cd6dded4fb4c65697288d64e95d5d33e
hitarth.rohra <hitarth.rohra@gmail.com>
2024-11-09 10:27:35 +0530

    Minor Updations

--- components/home/Home.jsx
+++ components/home/Home.jsx
@@ -25,7 +25,6 @@ const Home = () => {
                   "Writer",
                   "Photographer",
                   "General Enthusiast",
-
                 ]}
                 loop
                 typeSpeed={80}
