16e79a437590165893264fb6201b8dda3444e5c3
hitarth.rohra <hitarth.rohra@gmail.com>
2024-11-22 15:48:46 +0530

    updated shared social

--- components/social-share/ShareSocial.jsx
+++ components/social-share/ShareSocial.jsx
@@ -12,48 +12,52 @@ import {
   TumblrIcon,
 } from "next-share";
 
-const ShareSocial = () => {
+const ShareSocial = ({ blogData }) => {
+  // Default content if no blog data is passed
+  const shareUrl = blogData?.link || "https://hitaarthh.github.io";
+  const shareTitle = blogData?.title || "Hitarth's Blog";
+  const shareDescription = blogData?.descriptions
+    ? blogData.descriptions.replace(/<[^>]*>/g, '').slice(0, 200) + '...'
+    : "Check out this insightful blog post from Hitarth Anand Rohra, covering software engineering, technology, and personal experiences.";
+
   return (
     <div className="social-share">
       <FacebookShareButton
-        url={"https://www.facebook.com/ibthemes/"}
-        quote={
-          "We are ib-themes, a quality and dedicated group of individuals who love modern trend like React, Vue and Angular JS Web Templates. We are growing with Modern Web Technologies."
-        }
-        hashtag={"#ibthemes"}
+        url={shareUrl}
+        quote={shareDescription}
+        hashtag={"#TechBlog"}
       >
         <FacebookIcon size={32} round />
       </FacebookShareButton>
 
       <PinterestShareButton
-        url={"https://www.pinterest.com/"}
-        media={
-          "We are ib-themes, a quality and dedicated group of individuals who love modern trend like React, Vue and Angular JS Web Templates. We are growing with Modern Web Technologies."
-        }
+        url={shareUrl}
+        media={blogData?.img || "https://hitaarthh.github.io/img/logo/logo.png"}
+        description={shareTitle}
       >
         <PinterestIcon size={32} round />
       </PinterestShareButton>
 
       <TwitterShareButton
-        url={"https://twitter.com/ib_themes"}
-        title={
-          "We are ib-themes, a quality and dedicated group of individuals who love modern trend like React, Vue and Angular JS Web Templates. We are growing with Modern Web Technologies."
-        }
+        url={shareUrl}
+        title={shareTitle}
       >
         <TwitterIcon size={32} round />
       </TwitterShareButton>
 
       <LinkedinShareButton
-        url={"https://www.linkedin.com/in/ib-themes-company-469408207/"}
+        url={shareUrl}
+        title={shareTitle}
+        summary={shareDescription}
       >
         <LinkedinIcon size={32} round />
       </LinkedinShareButton>
 
       <TumblrShareButton
-        url={"https://www.tumblr.com/"}
-        title={
-          "We are ib-themes, a quality and dedicated group of individuals who love modern trend like React, Vue and Angular JS Web Templates. We are growing with Modern Web Technologies."
-        }
+        url={shareUrl}
+        title={shareTitle}
+        caption={shareDescription}
+        tags={["technology", "programming", "engineering"]}
       >
         <TumblrIcon size={32} round />
       </TumblrShareButton>
