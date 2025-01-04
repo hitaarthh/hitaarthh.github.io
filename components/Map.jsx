3e007f89de10ad3334e51a99f4ddb311f02b0b02
hitarth-pyi <hitarth@py-insights.com>
2025-01-04 13:10:48 +0530

    Location updated

--- components/Map.jsx
+++ components/Map.jsx
@@ -3,7 +3,7 @@ const Map = () => {
     <div className="map_wrap">
       <div className="map" style={{ height: "400px" }}>
         {
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.6684482859255!2d76.48924447526615!3d9.093936990970038!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b0602e7b4a2049d%3A0x3af3246b5138db0f!2sAmrita%20Vishwa%20Vidyapeetham%2C%20Amritapuri!5e0!3m2!1sen!2sin!4v1731162012310!5m2!1sen!2sin" allowFullScreen=""
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d114312.51537690093!2d80.25581112278523!3d26.447149974079892!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399c4770b127c46f%3A0x1778302a9fbe7b41!2sKanpur%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1735976264426!5m2!1sen!2sin" allowFullScreen=""
         loading="lazy"
         >/</iframe>}
       </div>
@@ -12,3 +12,5 @@ const Map = () => {
 };
 
 export default Map;
+
+
